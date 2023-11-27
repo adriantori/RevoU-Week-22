b06eb9acfa902d4d4acf642f97973f2fc7a57534
Adri Antori <adriantori11@gmail.com>
2023-11-27 14:44:34 +0700

    finished transformation from ExpressJS to Flask Py3

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+from flask_jwt_extended import JWTManager
+
+from db import db, db_init
+from auth.apis import auth_blueprint
+from todo.apis import todo_blueprint
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:smittywerbenjagermanjensen@db.cxebhyrtajkjbwvricio.supabase.co:5432/postgres"
+db.init_app(app)
+app.config['JWT_SECRET_KEY'] = "watzittooya"
+jwt = JWTManager(app)
+
+app.register_blueprint(auth_blueprint, url_prefix="/auth")
+app.register_blueprint(todo_blueprint, url_prefix="/todo")
+
+# with app.app_context():
+#     db_init()
